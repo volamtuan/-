002022e0e34141eeb8f8e278900a8a2b89cd904f
️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️ <30877561+volamtuan@users.noreply.github.com>
2024-05-23 21:34:32 +0700

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -14,22 +14,23 @@ gen64() {
 	echo "$1:$(ip64):$(ip64):$(ip64):$(ip64)"
 }
 install_3proxy() {
-    URL="https://github.com/3proxy/3proxy/archive/refs/tags/0.9.4.tar.gz"
+    echo "installing 3proxy"
+    URL="https://github.com/z3APA3A/3proxy/archive/3proxy-0.8.6.tar.gz"
     wget -qO- $URL | bsdtar -xvf-
-    cd 3proxy-0.9.4
+    cd 3proxy-3proxy-0.8.6
     make -f Makefile.Linux
-    mkdir -p /usr/local/etc/3proxy/{bin,stat}
-    cp bin/3proxy /usr/local/etc/3proxy/bin/
-    cp ../init.d/3proxy.sh /etc/init.d/3proxy
-    chmod +x /etc/init.d/3proxy
-    chkconfig 3proxy on
+    mkdir -p /usr/local/etc/3proxy/{bin,logs,stat}
+    cp src/3proxy /usr/local/etc/3proxy/bin/
+    #cp ./scripts/rc.d/proxy.sh /etc/init.d/3proxy
+    #chmod +x /etc/init.d/3proxy
+    #chkconfig 3proxy on
     cd $WORKDIR
 }
 
 gen_3proxy() {
     cat <<EOF
 daemon
-maxconn 4000
+maxconn 2000
 nserver 1.1.1.1
 nserver 8.8.4.4
 nserver 2001:4860:4860::8888
@@ -40,12 +41,9 @@ setgid 65535
 setuid 65535
 stacksize 6291456 
 flush
-auth strong
 
-users $(awk -F "/" 'BEGIN{ORS="";} {print $1 ":CL:" $2 " "}' ${WORKDATA})
-
-$(awk -F "/" '{print "auth strong\n" \
-"allow " $1 "\n" \
+$(awk -F "/" '{print "\n" \
+"" $1 "\n" \
 "proxy -6 -n -a -p" $4 " -i" $3 " -e"$5"\n" \
 "flush\n"}' ${WORKDATA})
 EOF
@@ -57,14 +55,9 @@ $(awk -F "/" '{print $3 ":" $4 ":" $1 ":" $2 }' ${WORKDATA})
 EOF
 }
 
-download_proxy() {
-    cd $WORKDIR || return
-    curl -F "file=@proxy.txt" https://file.io
-}
-
 gen_data() {
     seq $FIRST_PORT $LAST_PORT | while read port; do
-        echo "user$port/$(random)/$IP4/$port/$(gen64 $IP6)"
+        echo "//$IP4/$port/$(gen64 $IP6)"
     done
 }
 
@@ -79,6 +72,17 @@ gen_ifconfig() {
 $(awk -F "/" '{print "ifconfig eth0 inet6 add " $5 "/64"}' ${WORKDATA})
 EOF
 }
+
+cat << EOF > /etc/rc.d/rc.local
+#!/bin/bash
+touch /var/lock/subsys/local
+EOF
+
+echo "installing apps"
+yum -y install wget gcc net-tools bsdtar zip >/dev/null
+
+install_3proxy
+
 rotate_count=0
 
 rotate_ipv6() {
@@ -99,28 +103,27 @@ setup_cron_job() {
     rm mycron
 }
 
-echo "installing apps"
-yum -y install gcc net-tools bsdtar zip >/dev/null
-
-install_3proxy
-
-echo "working folder = /home/bkns"
-WORKDIR="/home/bkns"
+download_proxy() {
+    cd $WORKDIR || exit 1
+    curl -F "proxy.txt" https://transfer.sh
+}
+echo "working folder = /home/cloudfly"
+WORKDIR="/home/cloudfly"
 WORKDATA="${WORKDIR}/data.txt"
 mkdir $WORKDIR && cd $_
 
 IP4=$(curl -4 -s icanhazip.com)
 IP6=$(curl -6 -s icanhazip.com | cut -f1-4 -d':')
 
-echo "Internal ip = ${IP4}. External sub for ip6 = ${IP6}"
+echo "Internal ip = ${IP4}. Exteranl sub for ip6 = ${IP6}"
 
-FIRST_PORT=30000
-LAST_PORT=30333
+FIRST_PORT=20000
+LAST_PORT=20222
 
 gen_data >$WORKDIR/data.txt
 gen_iptables >$WORKDIR/boot_iptables.sh
 gen_ifconfig >$WORKDIR/boot_ifconfig.sh
-chmod +x boot_*.sh /etc/rc.local
+chmod +x $WORKDIR/boot_*.sh /etc/rc.local
 
 gen_3proxy >/usr/local/etc/3proxy/3proxy.cfg
 
@@ -131,23 +134,48 @@ ulimit -n 10048
 /usr/local/etc/3proxy/bin/3proxy /usr/local/etc/3proxy/3proxy.cfg
 EOF
 
-# Create rotate_ipv6 script
+bash /etc/rc.local
+
+gen_proxy_file_for_user
+rm -rf /root/3proxy-3proxy-0.8.6
+
 cat >$WORKDIR/rotate_ipv6.sh <<EOF
 #!/bin/sh
 $(declare -f rotate_ipv6)
 rotate_ipv6
 EOF
 chmod +x $WORKDIR/rotate_ipv6.sh
-
 # Setup cron job for rotating IPv6 every 10 minutes
 setup_cron_job
 
-bash /etc/rc.local
-
-gen_proxy_file_for_user
-rm -rf /root/3proxy-0.9.4
-
 echo "Starting Proxy"
+
 echo "So Luong IPv6 Hien Tai:"
 ip -6 addr | grep inet6 | wc -l
-download_proxy
+
+while true; do
+    echo "1. Reset 3proxy Setup"
+    echo "2. Rotate IPv6"
+    echo "3. Download proxy"
+    echo "4. Exit"
+    echo -n "Enter your choice: "
+    read choice
+    case $choice in
+        1)
+            install_3proxy
+            ;;
+        2)
+            rotate_ipv6
+            ;;
+        3)
+            download_proxy
+            ;;
+        4)
+            echo "Exiting..."
+            exit 0
+            ;;
+        *)
+            echo "Invalid choice. Please try again."
+            ;;
+    esac
+done
