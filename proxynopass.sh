d2bbff0d29172b986dc5a6ec2ea03839cfd52cd7
️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️ <30877561+volamtuan@users.noreply.github.com>
2024-06-05 22:47:03 +0700

    Update proxynopass.sh

--- proxynopass.sh
+++ proxynopass.sh
@@ -84,20 +84,20 @@ install_3proxy() {
 gen_3proxy() {
     cat <<EOF
 daemon
-maxconn 2000
+maxconn 4000
 nserver 1.1.1.1
-nserver 8.8.4.4
+nserver 8.8.8.8
 nserver 2001:4860:4860::8888
 nserver 2001:4860:4860::8844
 nscache 65536
 timeouts 1 5 30 60 180 1800 15 60
 setgid 65535
 setuid 65535
-stacksize 6291456 
+stacksize 6291456
 flush
+auth none
 
-$(awk -F "/" '{print "\n" \
-"" $1 "\n" \
+$(awk -F "/" '{print "allow *\n" \
 "proxy -6 -n -a -p" $4 " -i" $3 " -e"$5"\n" \
 "flush\n"}' ${WORKDATA})
 EOF
