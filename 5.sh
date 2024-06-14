5cc15c97cbaaec6be351f3d0e961846d16488362
️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️ <30877561+volamtuan@users.noreply.github.com>
2024-06-14 14:43:35 +0700

    Update 5.sh

--- 5.sh
+++ 5.sh
@@ -27,22 +27,37 @@ gen64() {
 }
 
 # Cài đặt 3proxy
+
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+
+random() {
+    tr </dev/urandom -dc A-Za-z0-9 | head -c5
+    echo
+}
+
+array=(1 2 3 4 5 6 7 8 9 0 a b c d e f)
+gen64() {
+    ip64() {
+        echo "${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}"
+    }
+    echo "$1:$(ip64):$(ip64):$(ip64):$(ip64)"
+}
+
 install_3proxy() {
-    echo "Đang cài đặt 3proxy..."
-    URL="https://github.com/z3APA3A/3proxy/archive/refs/tags/0.9.3.tar.gz"
-    wget -qO- $URL | tar -xz
-    cd 3proxy-0.9.3
+    echo "Installing 3proxy..."
+    URL="https://github.com/z3APA3A/3proxy/archive/3proxy-0.8.6.tar.gz"
+    wget -qO- $URL | bsdtar -xvf- >/dev/null 2>&1
+    cd 3proxy-3proxy-0.8.6
     make -f Makefile.Linux >/dev/null 2>&1
     mkdir -p /usr/local/etc/3proxy/{bin,logs,stat} >/dev/null 2>&1
     cp src/3proxy /usr/local/etc/3proxy/bin/ >/dev/null 2>&1
     cd $WORKDIR
 }
 
-# Sinh cấu hình 3proxy
 gen_3proxy() {
     cat <<EOF
 daemon
-maxconn 5000
+maxconn 10048
 nserver 1.1.1.1
 nserver 8.8.4.4
 nserver 2001:4860:4860::8888
@@ -51,16 +66,17 @@ nscache 65536
 timeouts 1 5 30 60 180 1800 15 60
 setgid 65535
 setuid 65535
-stacksize 6291456
+stacksize 6291456 
 flush
-auth iponly
+authcache user 86400
+auth none cache
+auth iponly cache
 allow 14.224.163.75
-allow 127.0.0.1
 deny *
 
 $(awk -F "/" '{print "\n" \
 "allow *" $1 "\n" \
-"proxy -6 -n -a -p" $4 " -i" $3 " -e" $5 "\n" \
+"proxy -6 -n -a -p" $4 " -i" $3 " -e"$5"\n" \
 "flush\n"}' ${WORKDATA})
 EOF
 }
@@ -123,7 +139,7 @@ bash /etc/rc.local
 
 # Xóa thư mục tạm của 3proxy
 echo "Đang xóa thư mục tạm của 3proxy..."
-rm -rf /root/3proxy-0.9.3
+rm -rf /root/3proxy-3proxy-0.8.6
 
 # Sinh file proxy cho người dùng
 echo "Đang sinh file proxy cho người dùng..."
