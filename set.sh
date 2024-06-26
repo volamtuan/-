815550720ceee6ce78da4ca8f8847214bbdccd8b
️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️ <30877561+volamtuan@users.noreply.github.com>
2024-06-26 17:33:30 +0700

    Update set.sh

--- set.sh
+++ set.sh
@@ -3,7 +3,6 @@
 # Thiết lập biến PATH
 PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 
-# Hàm thiết lập IPv6
 setup_ipv6() {
     echo "Thiết lập IPv6..."
     ip -6 addr flush dev eth0
@@ -13,106 +12,61 @@ setup_ipv6() {
 # Gọi hàm thiết lập IPv6
 setup_ipv6
 
-# Cài đặt các gói cần thiết
-yum install make wget curl jq git iptables-services -y
-
-# Lấy tên interface mạng
-NETWORK_INTERFACE_NAME=$(ip route get 8.8.8.8 | sed -nr 's/.*dev ([^\ ]+).*/\1/p')
-echo "MTU=1500" >> /etc/sysconfig/network-scripts/ifcfg-$NETWORK_INTERFACE_NAME
-
-# Kiểm tra hệ thống có sử dụng YUM hay không
-YUM=$(which yum)
-
-if [ "$YUM" ]; then
-    echo > /etc/sysctl.conf
-    tee -a /etc/sysctl.conf <<EOF
-net.ipv6.conf.default.disable_ipv6 = 0
-net.ipv6.conf.all.disable_ipv6 = 0
-EOF
-    sysctl -p
-
-    interfaces=$(ip -o link show | awk -F': ' '{print $2}')
-    IPC=$(curl -4 -s icanhazip.com | cut -d"." -f3)
-    IPD=$(curl -4 -s icanhazip.com | cut -d"." -f4)
-    for iface in $interfaces; do
-        if [ $IPC == 4 ]; then
-            tee -a /etc/sysconfig/network-scripts/ifcfg-$iface <<-EOF
-IPV6INIT=yes
-IPV6_AUTOCONF=no
-IPV6_DEFROUTE=yes
-IPV6_FAILURE_FATAL=no
-IPV6_ADDR_GEN_MODE=stable-privacy
-IPV6ADDR=2403:6a40:0:40::$IPD:0000/64
-IPV6_DEFAULTGW=2403:6a40:0:40::1
-EOF
-        elif [ $IPC == 5 ]; then
-            tee -a /etc/sysconfig/network-scripts/ifcfg-$iface <<-EOF
-IPV6INIT=yes
-IPV6_AUTOCONF=no
-IPV6_DEFROUTE=yes
-IPV6_FAILURE_FATAL=no
-IPV6_ADDR_GEN_MODE=stable-privacy
-IPV6ADDR=2403:6a40:0:41::$IPD:0000/64
-IPV6_DEFAULTGW=2403:6a40:0:41::1
-EOF
-        elif [ $IPC == 244 ]; then
-            tee -a /etc/sysconfig/network-scripts/ifcfg-$iface <<-EOF
-IPV6INIT=yes
-IPV6_AUTOCONF=no
-IPV6_DEFROUTE=yes
-IPV6_FAILURE_FATAL=no
-IPV6_ADDR_GEN_MODE=stable-privacy
-IPV6ADDR=2403:6a40:2000:244::$IPD:0000/64
-IPV6_DEFAULTGW=2403:6a40:2000:244::1
-EOF
-        else
-            tee -a /etc/sysconfig/network-scripts/ifcfg-$iface <<-EOF
-IPV6INIT=yes
-IPV6_AUTOCONF=no
-IPV6_DEFROUTE=yes
-IPV6_FAILURE_FATAL=no
-IPV6_ADDR_GEN_MODE=stable-privacy
-IPV6ADDR=2403:6a40:0:$IPC::$IPD:0000/64
-IPV6_DEFAULTGW=2403:6a40:0:$IPC::1
-EOF
-        fi
-    done
-
-    service network restart
-
-# Nếu không sử dụng YUM thì kiểm tra sử dụng apt (Ubuntu)
+# Kiểm tra và cài đặt các gói cần thiết sử dụng YUM hoặc APT
+if command -v yum &> /dev/null; then
+    yum install -y make wget curl jq git iptables-services
+elif command -v apt-get &> /dev/null; then
+    apt-get install -y make wget curl jq git iptables-services
 else
-    ipv4=$(curl -4 -s icanhazip.com)
-    IPC=$(curl -4 -s icanhazip.com | cut -d"." -f3)
-    IPD=$(curl -4 -s icanhazip.com | cut -d"." -f4)
-    INT=$(ls /sys/class/net | grep e)
-    if [ "$IPC" = "4" ]; then
-        IPV6_ADDRESS="2403:6a40:0:40::$IPD:0000/64"
-        GATEWAY="2403:6a40:0:40::1"
-    elif [ "$IPC" = "5" ]; thì
-        IPV6_ADDRESS="2403:6a40:0:41::$IPD:0000/64"
-        GATEWAY="2403:6a40:0:41::1"
-    elif [ "$IPC" = "244" ]; thì
-        IPV6_ADDRESS="2403:6a40:2000:244::$IPD:0000/64"
-        GATEWAY="2403:6a40:2000:244::1"
-    else
-        IPV6_ADDRESS="2403:6a40:0:$IPC::$IPD:0000/64"
-        GATEWAY="2403:6a40:0:$IPC::1"
-    fi
-    interface_name="$INT"  # Tên giao diện mạng
-    ipv6_address="$IPV6_ADDRESS"
-    gateway6_address="$GATEWAY"
-    netplan_path="/etc/netplan/50-cloud-init.yaml"  # Đường dẫn tập tin cấu hình Netplan
-
-    netplan_config=$(cat "$netplan_path")
-    new_netplan_config=$(sed "/gateway4:/i \ \ \ \ \ \ \ \ \ \ \ \ - $ipv6_address" <<< "$netplan_config")
-    new_netplan_config=$(sed "/gateway4:.*/a \ \ \ \ \ \ \ \ \ \ \ \ gateway6: $gateway6_address" <<< "$new_netplan_config")
-
-    echo "$new_netplan_config" > "$netplan_path"
-
-    sudo netplan apply
+    echo "Không tìm thấy YUM hoặc APT để cài đặt gói."
+    exit 1
 fi
 
+# Lấy tên interface mạng
+NETWORK_INTERFACE_NAME=$(ip route get 8.8.8.8 | awk '{print $5}')
+
+# Cấu hình các file ifcfg-* trong /etc/sysconfig/network-scripts/
+configure_network_scripts() {
+    IPC=$(echo $(curl -4 -s icanhazip.com | cut -d"." -f3))
+    IPD=$(echo $(curl -4 -s icanhazip.com | cut -d"." -f4))
+    
+    interfaces=$(ip -o link show | awk -F': ' '{print $2}')
+    
+    for iface in $interfaces; do
+        case $IPC in
+            4)
+                IPV6_ADDRESS="2403:6a40:0:40::$IPD:0000/64"
+                IPV6_DEFAULTGW="2403:6a40:0:40::1"
+                ;;
+            5)
+                IPV6_ADDRESS="2403:6a40:0:41::$IPD:0000/64"
+                IPV6_DEFAULTGW="2403:6a40:0:41::1"
+                ;;
+            244)
+                IPV6_ADDRESS="2403:6a40:2000:244::$IPD:0000/64"
+                IPV6_DEFAULTGW="2403:6a40:2000:244::1"
+                ;;
+            *)
+                IPV6_ADDRESS="2403:6a40:0:$IPC::$IPD:0000/64"
+                IPV6_DEFAULTGW="2403:6a40:0:$IPC::1"
+                ;;
+        esac
+        
+        tee -a /etc/sysconfig/network-scripts/ifcfg-$iface <<-EOF
+            IPV6INIT=yes
+            IPV6_AUTOCONF=no
+            IPV6_DEFROUTE=yes
+            IPV6_FAILURE_FATAL=no
+            IPV6_ADDR_GEN_MODE=stable-privacy
+            IPV6ADDR=$IPV6_ADDRESS
+            IPV6_DEFAULTGW=$IPV6_DEFAULTGW
+EOF
+    done
+    
+    systemctl restart network
+}
+
+configure_network_scripts
 echo 'IPv6 đã được cấu hình thành công!'
 
 # Hàm random để tạo chuỗi ngẫu nhiên
